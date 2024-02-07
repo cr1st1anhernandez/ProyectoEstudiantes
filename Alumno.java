4eee90d35074cfbb7982359b01bae6632121365a
Cristian Neftali Hernandez Ramirez <cristian0402218@gmail.com>
2024-02-06 20:44:48 -0600

    :sparkles: Add class Alumno

---
+++ Alumno.java
@@ -0,0 +1,62 @@
+public class Alumno {
+  private String nombre;
+  private int edad;
+  private char genero;
+  private String numeroDeControl;
+  private String apellidoPaterno;
+  private String apellidoMaterno;
+
+  public Alumno(String nombre, int edad, char genero, String numeroDeControl, String apellidoPaterno, String apellidoMaterno) {
+    this.nombre = nombre;
+    this.genero = genero;
+    this.edad = edad;
+    this.numeroDeControl = numeroDeControl;
+    this.apellidoPaterno = apellidoPaterno;
+    this.apellidoMaterno = apellidoMaterno;
+  }
+
+  public void setApellidoMaterno(String apellidoMaterno) {
+    this.apellidoMaterno = apellidoMaterno;
+  }
+
+  public void setApellidoPaterno(String apellidoPaterno) {
+    this.apellidoPaterno = apellidoPaterno;
+  }
+
+  public void setEdad(int edad) {
+    this.edad = edad;
+  }
+
+  public void setGenero(char genero) {
+    this.genero = genero;
+  }
+
+  public void setNombre(String nombre) {
+    this.nombre = nombre;
+  }
+
+  public void setNumeroDeControl(String numeroDeControl) {
+    this.numeroDeControl = numeroDeControl;
+  }
+
+  public int getEdad() {
+    return edad;
+  }
+
+  public char getGenero() {
+    return genero;
+  }
+
+  public String getNombre() {
+    return nombre;
+  }
+
+  public String getNumeroDeControl() {
+    return numeroDeControl;
+  }
+
+  public String toString() {
+    return "Número de control: " + numeroDeControl + "\nNombre: " + nombre + "\nApellido paterno: " + apellidoPaterno
+        + "\nApellido materno: " + apellidoMaterno + "\nGénero: " + genero + "\nEdad: " + edad;
+  }
+}
